e2a73d71629b5e0e462036394ce0d20e01c3d4af
KARDIINAAl <ramemlkxv6@gmail.com>
2025-05-19 10:52:27 +0100

    Update its_justlearning.java
    ok I have create a new function that staic to the class and did'nt make any object for it yet

--- its_justlearning.java
+++ its_justlearning.java
@@ -1,7 +1,11 @@
 //this my first tuotrial in github
 public   class maIN {
+   static void myname(){
+         System.out.Println("my name is omer");
+    }
     public static void main(String[] args) {
       System.out.Println("hi from the otherside");
+        
 
     }
 }
